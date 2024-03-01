109fed172f20087a4ab82ada04252a379d941f4d
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-01 15:26:11 +0800

    Update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -34,6 +34,9 @@ function main {
         #
         for batch_size in ${batch_size_list[@]}
         do
+            if [ $batch_size -le 0 ];then
+                batch_size=144
+            fi
             # clean workspace
             logs_path_clean
             # generate launch script for multiple instance
